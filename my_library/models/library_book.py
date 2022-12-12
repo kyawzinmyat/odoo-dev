bb2d3b9fd39e99070992e1befa141983db207ff3
kyawzinmyat21 <kyawzinmyat@global-conntect.asia>
2022-12-12 16:46:46 +0630

    library

---
+++ my_library/models/library_book.py
@@ -0,0 +1,31 @@
+from odoo import fields, models
+
+class LibraryBook(models.Model):
+    _name = "library.book"
+    _description = 'Library Book'
+    _order = "date_release desc, name"
+    _rec_name = 'short_name'
+
+    name = fields.Char("Title", required = True)
+    author_ids = fields.Many2many(
+        'res.parnter',
+        string = "Authors"
+    )
+
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+            [('draft', 'Not Available'),
+            ('available', 'Available'),
+            ('lost', 'Lost')],
+            'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float(
+                    'Reader Average Rating',
+                    digits=(14, 4), # Optional precision decimals,
+                    )
+    short_name = fields.Char('Short Title', required=True)
