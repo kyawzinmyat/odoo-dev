c9333ecd2eba17dbf332f815175b14d422c99f02
kyawzinmyat <kyawzinmyat@global-connect.asia>
2022-12-15 23:19:20 +0630

    Lib

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -1,5 +1,8 @@
 from odoo import fields, models, api
 from datetime import timedelta
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
+
 
 class LibraryBook(models.Model):
     _name = "library.book"
@@ -21,8 +24,8 @@ class LibraryBook(models.Model):
     state = fields.Selection(
             [('draft', 'Not Available'),
             ('available', 'Available'),
-            ('lost', 'Lost')],
-            'State')
+            ('lost', 'Lost'), ('borrowed', 'Borrowed')],
+            'State', default = 'draft')
     description = fields.Html('Description')
     cover = fields.Binary('Book Cover')
     out_of_print = fields.Boolean('Out of Print?')
@@ -41,13 +44,14 @@ class LibraryBook(models.Model):
     category_id = fields.Many2one('library.book.category')
     age_days = fields.Integer(string = 'Day since Release', compute = "_compute_age", inverse = "_inverse_age", search = "_search_age")
     publisher_city = fields.Char('Publisher City', related='publisher_id.city', readonly=True)
-    
+    author_ids = fields.Many2many('res.partner', string='Authors')
     
     @api.constrains('date_release')
     def _check_release_date(self):
         for record in self:
             if record.date_release and record.date_release > fields.Date.today():
                 raise models.ValidationError("Error in the date release")
+
     @api.depends('date_release')
     def _compute_age(self):
         for record in self:
@@ -55,10 +59,44 @@ class LibraryBook(models.Model):
                 record.age_days = (fields.Date.today() - record.date_release).days
             else:
                 record.age_days = 0
-        
+    @api.model
+    def is_allowed_transition(self, old_state, new_state):
+        print(old_state)
+        allowed = [('draft', 'available'),
+                    ('available', 'borrowed'),
+                    ('borrowed', 'available'),
+                    ('available', 'lost'),
+                    ('borrowed', 'lost'),
+                    ('lost', 'available')]
+        return (old_state, new_state) in allowed
+    
+    def change_state(self, new_state):
+        for book in self:
+            if book.is_allowed_transition(book.state, new_state):
+                book.state = new_state
+            else:
+                msg = _("Moving from %s to %s is not allowed") % (book.state, new_state)
+                raise UserError(msg)
+            
+    def make_available(self):
+        self.change_state("available")
+
+    def make_borrowed(self):
+        self.change_state("borrowed")
+    
+    def make_lost(self):
+        self.change_state("lost")
+
+
     def _inverse_age(self):
         for record in self:
             record.date_release = fields.Date.today() - timedelta(days = record.age_days)
+
+    def log_all_library_members(self):
+        library_member_model = self.env['library.member']
+        all_members = library_member_model.search([])
+        print("ALL MEMBERS:", all_members)
+        return True
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -71,10 +109,14 @@ class ResPartner(models.Model):
     def _compute_count_books(self):
         for record in self:
             self.count_books = len(self.authored_book_ids)
+            
         
 
 class LibraryMember(models.Model):
-    _inherits = {'res.partner' : 'partner_id'}
     _name = 'library.member'
-
-    partner_id = fields.Many2many('res.partner', ondelete = "cascade")
+    _inherits = {'res.partner' : 'partner_id'}
+    partner_id = fields.Many2one('res.partner', ondelete = "cascade")
+    date_start = fields.Date('Member Since')
+    date_end = fields.Date('Termination Date')
+    member_number = fields.Char()
+    date_of_birth = fields.Date('Date of birth')
